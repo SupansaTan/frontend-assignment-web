38c26b35c60fe889587d3223ca94ce327eb0ce0f
Supansa <s6201012620236@email.kmutnb.ac.th>
2022-01-13 13:20:17 +0700

    edit title, description and tags on trip card

--- src/components/trip-card/trip-card.tsx
+++ src/components/trip-card/trip-card.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Row, Col, Image } from 'react-bootstrap';
+import { Card, Row, Col, Image, Button } from 'react-bootstrap';
+import ShowMoreText from "react-show-more-text";
 import { TripModel } from '../../model/trip.model';
 import { environment } from '../../environments/environment';
+import './trip-card.scss';
 
 function TripCardComponent() {
   const [trips, setTrips] = useState<Array<TripModel>>([])
@@ -11,7 +13,7 @@ function TripCardComponent() {
       try {
         const response = await fetch(`${environment.apiGateway.URL}/api/trips`);
         const data = await response.json();
-        setTrips(data)
+        setTrips(data.trips)
       } 
       catch (error) {
         console.log(error);
@@ -28,18 +30,50 @@ function TripCardComponent() {
           return(
             <Card key={trip.eid} className="border-0 my-4">
               <Row>
-                <Col>
+                <Col xs="auto">
                   <Image src={trip.photos[0]} width={150} height={200} className="rounded-3" alt={trip.title}/>
                 </Col>
+
                 <Col>
-                  <h4>{ trip.title }</h4>
-                  <p>
-                    { trip.description } 
+                  <a href={trip.url} className="text-decoration-none text-black">
+                    <h4 className="fw-bold">{ trip.title }</h4>
+                  </a>
+
+                  {/* description */}
+                  <ShowMoreText lines={4} truncatedEndingComponent={" ... "} more={'อ่านต่อ'} className="text-muted">
+                    {trip.description}
                     <a href={trip.url}>อ่านต่อ</a>
-                  </p>
+                  </ShowMoreText>
+
+                  {/* tags */}
+                  <div>
+                    <span className="me-2 mt-2">หมวด</span>
+                    <TagGroup tags={trip.tags}/>
+                  </div>
+
                 </Col>
               </Row>
             </Card>
+          )
+        })
+      }
+    </React.Fragment>
+  )
+}
+
+interface TagProps {
+  tags: Array<string>
+}
+
+const TagGroup = ({tags}: TagProps) => {
+  return(
+    <React.Fragment>
+      {
+        tags.map((tag: string, index: number) => {
+          return(
+            <Button key={tag} className={'rounded-3 text-black bg-lightgrey border-lightgrey rounded-25 shadow-none mt-2 tag-btn ' + ((index === 0)? 'me-1':'mx-1')}>
+              { tag }
+            </Button>
           )
         })
       }
