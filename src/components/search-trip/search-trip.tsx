a53f911ecb7cda2ec30719e378dfb9faa8085e65
Supansa <s6201012620236@email.kmutnb.ac.th>
2022-01-13 13:19:23 +0700

    add search input form

--- src/components/search-trip/search-trip.tsx
+++ src/components/search-trip/search-trip.tsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
 
 function SearchTripComponent() {
   const [searchTerms, setSearchTerms] = useState('')
 
   return(
     <React.Fragment>
-      
+      <Form.Control
+        type="search"
+        defaultValue={searchTerms}
+        onChange={(e) => {setSearchTerms(e.target.value)}}
+        placeholder="Search"
+        id="search_terms"
+        className="my-4 border-0 border-bottom rounded-0 shadow-none"
+        aria-label="SearchTrip"
+      />
     </React.Fragment>
   )
 }
