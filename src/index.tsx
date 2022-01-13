2ab27edc449499fed6f8ddf9f1931a010dbaf9c4
Supansa <s6201012620236@email.kmutnb.ac.th>
2022-01-13 13:10:01 +0700

    edit heading color and install bootstrap icon

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import SearchTripComponent from '../components/search-trip/search-trip';
 import TripCardComponent from '../components/trip-card/trip-card';
+import './index.scss';
 
 function HomePage() {
   return(
     <React.Fragment>
       <Row>
-        <h1 className="text-center">เที่ยวไหนดี</h1>
+        <h1 className="text-center heading text-blue">เที่ยวไหนดี</h1>
 
         <SearchTripComponent/>
         <TripCardComponent/>
