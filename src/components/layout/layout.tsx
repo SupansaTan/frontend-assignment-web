9ad9bb1a86f4598a5c031a800320d5c1e56a3dd2
Supansa <s6201012620236@email.kmutnb.ac.th>
2022-01-14 21:44:57 +0700

    show navbar when scroll down

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Container, Navbar } from 'react-bootstrap';
 
 interface Props {
@@ -6,9 +6,29 @@ interface Props {
 }
 
 function LayoutComponent({ children }: Props) {
+  const [isScroll, setIsScroll] = useState(false)
+
+  const handleScroll = useCallback((event) => {
+    let scrollTop = event.target.scrollingElement.scrollTop
+    if(scrollTop >= 20) {
+      setIsScroll(true)
+    }
+    else {
+      setIsScroll(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, true)
+
+    return() => {
+      window.removeEventListener("scroll", handleScroll, true)
+    }
+  }, [handleScroll])
+
   return(
     <React.Fragment>
-      <Navbar className="bg-blue" sticky="top">
+      <Navbar className={"bg-blue " + (isScroll? '':'d-none')} sticky="top">
         <Container>
           <Navbar.Brand href="/" className="fw-bold text-white">เที่ยวไหนดี</Navbar.Brand>
         </Container>
