c33c291b72d6fbd0d4adc853a9e18c0f7713b9ae
Supansa <s6201012620236@email.kmutnb.ac.th>
2022-01-15 13:07:21 +0700

    add toast message

--- src/components/toast/toast.tsx
+++ src/components/toast/toast.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Toast, ToastContainer } from 'react-bootstrap'
 
 interface Props {
+  status: string;
   message: string
 }
 
@@ -10,17 +11,13 @@ function ToastComponent(props: Props) {
     <React.Fragment>
       <ToastContainer className="p-3" position={'top-center'}>
         <Toast>
-          <Toast.Header closeButton={true}>
-            <img
-              src="holder.js/20x20?text=%20"
-              className="rounded me-2"
-              alt=""
-            />
-            <strong className="me-auto">Bootstrap</strong>
-          </Toast.Header>
-          <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+          <Toast.Body>
+            { props.message }
+          </Toast.Body>
         </Toast>
       </ToastContainer>
     </React.Fragment>
   )
 }
+
+export default ToastComponent;
