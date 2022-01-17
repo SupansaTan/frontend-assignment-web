32e31ba9b956441e75ebf1f57e755cc1944e22ab
Supansa <s6201012620236@email.kmutnb.ac.th>
2022-01-17 07:37:21 +0700

    change search text context

--- src/context/search-text.tsx
+++ src/context/search-text.tsx
@@ -6,29 +6,23 @@ type Props = {
 
 type Context = {
   searchText: string;
-  isSubmit: boolean;
   changeSearchText: (inputText: string) => void;
-  changeIsSubmit: () => void;
 }
 
 const contextDefault: Context = {
   searchText: '',
-  isSubmit: false,
   changeSearchText: () => {},
-  changeIsSubmit: () => {}
 }
 
 export const SearchTextContext = React.createContext<Context>(contextDefault);
 
 export const SearchTextProvider = ({ children }: Props) => {
   const [searchText, setSearchText] = useState<string>('')
-  const [isSubmit, setIsSubmit] = useState<boolean>(false)
 
   const changeSearchText = (inputText: string) => setSearchText(inputText);
-  const changeIsSubmit = () => setIsSubmit(!isSubmit);
 
   return(
-    <SearchTextContext.Provider value={{ searchText, changeSearchText, isSubmit, changeIsSubmit }}>
+    <SearchTextContext.Provider value={{ searchText, changeSearchText }}>
       { children }
     </SearchTextContext.Provider>
   )
